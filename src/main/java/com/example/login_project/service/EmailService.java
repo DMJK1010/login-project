dbe84d8fe204eef3a70791faf19e62586d38b86f
DMJK1010 <jon001.aa9@gmail.com>
2025-11-25 15:17:34 +0900

    add send mail service

---
+++ src/main/java/com/example/login_project/service/EmailService.java
@@ -0,0 +1,22 @@
+package com.example.login_project.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String toEmail, String title, String text){
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(toEmail);
+        message.setSubject(title);
+        message.setText(text);
+
+        mailSender.send(message);
+    }
+}
